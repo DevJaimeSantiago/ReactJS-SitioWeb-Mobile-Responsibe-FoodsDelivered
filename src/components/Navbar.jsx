e9503a483171ea592693ba8765692bff26c56488
DevJaimeSantiago <jaimestgo475@gmail.com>
2023-07-03 12:01:33 -0500

    navbar Finalizado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,29 @@
-import { AiFillTag, AiOutlineClose, AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+import { useState } from "react";
+
+// icons
+import {
+	AiFillTag,
+	AiOutlineClose,
+	AiOutlineMenu,
+	AiOutlineSearch,
+} from "react-icons/ai";
 import { BsFillCartFill, BsFillSafeFill } from "react-icons/bs";
 import { TbTruckDelivery } from "react-icons/tb";
 import { MdFavorite, MdHelp } from "react-icons/md";
 import { FaUserFriends, FaWallet } from "react-icons/fa";
 
 const Navbar = () => {
+	const [nav, setNav] = useState(false);
+
 	return (
 		<>
 			<div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
 				{/* left size */}
 				<div className="flex items-center">
-					<div className="cursor-pointer">
+					<div
+						onClick={() => setNav(!nav)}
+						className="cursor-pointer"
+					>
 						<AiOutlineMenu size={30} />
 					</div>
 
@@ -45,13 +58,26 @@ const Navbar = () => {
 
 				{/* Mobile Menu */}
 				{/* Overlay */}
-				<div className="bg-black/80 fixed w-full h-screen top-0 left-0"></div>
+				{nav ? (
+					<div className="bg-black/80 fixed w-full h-screen top-0 left-0"></div>
+				) : (
+					""
+				)}
 
 				{/* Side drawer Menu */}
-				<div className="fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300">
+				<div
+					className={
+						nav
+							? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-500"
+							: "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-500"
+					}
+				>
 					<AiOutlineClose
 						size={30}
 						className="absolute top-4 right-4 cursor-pointer"
+						onClick={() => {
+							setNav(!nav);
+						}}
 					/>
 					<h2 className="text-2xl p-4">
 						Best <span className="font-bold">Eats</span>
@@ -63,55 +89,55 @@ const Navbar = () => {
 									size={25}
 									className="mr-4"
 								/>
-                                Orders
+								Orders
 							</li>
 
-                            <li className="text-xl py-4 flex">
+							<li className="text-xl py-4 flex">
 								<MdFavorite
 									size={25}
 									className="mr-4"
 								/>
-                                Favorites
+								Favorites
 							</li>
 
-                            <li className="text-xl py-4 flex">
+							<li className="text-xl py-4 flex">
 								<FaWallet
 									size={25}
 									className="mr-4"
 								/>
-                                Wallet
+								Wallet
 							</li>
 
-                            <li className="text-xl py-4 flex">
+							<li className="text-xl py-4 flex">
 								<MdHelp
 									size={25}
 									className="mr-4"
 								/>
-                                Help
+								Help
 							</li>
 
-                            <li className="text-xl py-4 flex">
+							<li className="text-xl py-4 flex">
 								<AiFillTag
 									size={25}
 									className="mr-4"
 								/>
-                                Promotions
+								Promotions
 							</li>
 
-                            <li className="text-xl py-4 flex">
+							<li className="text-xl py-4 flex">
 								<BsFillSafeFill
 									size={25}
 									className="mr-4"
 								/>
-                                Best ones
+								Best ones
 							</li>
 
-                            <li className="text-xl py-4 flex">
+							<li className="text-xl py-4 flex">
 								<FaUserFriends
 									size={25}
 									className="mr-4"
 								/>
-                                Invite Friends
+								Invite Friends
 							</li>
 						</ul>
 					</nav>
