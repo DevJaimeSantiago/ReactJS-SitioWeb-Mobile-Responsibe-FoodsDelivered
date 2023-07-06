08c53d4e35ee43fa09699cfc623af1480c79e270
DevJaimeSantiago <jaimestgo475@gmail.com>
2023-07-06 13:32:00 -0500

    componente category finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import HeadlineCard from "./components/HeadlineCard"
 import Food from "./components/Food"
+import Category from "./components/Category"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
 		<Hero />
 		<HeadlineCard />
 		<Food />
+		<Category/>
 	</div>
   )
 }
